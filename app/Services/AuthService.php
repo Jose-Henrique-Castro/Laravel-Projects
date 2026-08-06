c0b81cb1bf1ae8adfab49dd7a68ee64570ba8a8a
José Henrique <josephenrycastro@gmail.com>
2026-08-06 19:32:37 -0300

    feat: creat the metod register , login and logout

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -1,8 +1,43 @@
 <?php
 
 namespace App\Services;
+use Illuminate\Support\Facades\Hash;
+use app\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthService
 {
-    //
+    public function register(array $data){
+
+   $data['password'] = Hash::make($data['password']); // get the password and encrypt it
+
+   $user = User::create($data); // create a register of an User and put them all in the variable
+
+    Auth::login($user); // login the user after the registration
+
+    return $user; 
+
+    }
+
+
+    public function login (array $credentials){
+
+        $login = Auth::attempt($credentials); // get the attempt of login 
+
+        return $login; // just return the true/false answer
+
+    }
+
+
+    public function logout(){
+
+        Auth::logout(); // finish the session of the actual user
+
+        // invalidate the session and regenerate the token CSRF to prevent attacks of session fixation
+        request()->session()->invalidate(); 
+        request()->session()->regenerateToken(); 
+
+    }
+
+
 }
