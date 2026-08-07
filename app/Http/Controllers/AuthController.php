247f7fac8d5753183446f33329daff3991949342
José Henrique <josephenrycastro@gmail.com>
2026-08-07 16:00:10 -0300

    feat: Create AuthControler and their metods

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use app\Services\AuthService;
+
+class AuthController extends Controller
+{
+    
+
+public function __construct( private AuthService $authservice ) {} // inject the AuthService
+
+public function showRegister(){ return view('auto.register'); }
+
+public function showLogin() { return view('auto.login'); } // metods to return the views 
+
+public function Register(Request $request) { 
+
+    $validated = $request->validate([
+
+        'name' => ['required','string','max:255'],
+        'email' => ['required','string','unique:user,email'],
+        'password' => ['required','confirmed','min:8'],
+
+    ]);
+
+    $this->authservice->register($validated);
+
+    return redirect()->route('posts.index');
+
+}
+
+public function login(Request $request){
+
+    $validated = $request->validate([
+
+    'email' => ['required','email'],
+    'passoword' => ['required'],
+
+    ]);
+
+    if (! $this->authservice->login($validated)) { // if fail give an message and maintain the email field filled in
+        return back()->withErrors(['email' => 'Invalid credentials'])->onlyInput('email');
+    }
+
+    $request->session()->regenerate(); // avoid Session Fixation on login
+
+    return redirect()->route('posts.index');
+
+}
+
+
+public function logout() {
+
+    $this->authservice->logout();
+
+    return redirect()->route('login');
+
+}
+
+
+
+
+}
