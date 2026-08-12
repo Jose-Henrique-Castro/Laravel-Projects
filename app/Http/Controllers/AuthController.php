37b93d29cefed4e4d8fd3780978ce71a585cfce8
José Henrique <josephenrycastro@gmail.com>
2026-08-12 16:28:50 -0300

    fixed the name of the return views from showRegister and showLogin

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,9 +11,9 @@ class AuthController extends Controller
 
 public function __construct( private AuthService $authservice ) {} // inject the AuthService
 
-public function showRegister(){ return view('register'); }
+public function showRegister(){ return view('auth.register'); }
 
-public function showLogin() { return view('login'); } // metods to return the views 
+public function showLogin() { return view('auth.login'); } // metods to return the views 
 
 public function register(Request $request) { 
 
