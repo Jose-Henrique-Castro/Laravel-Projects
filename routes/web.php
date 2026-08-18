ce87079b1eae3649876c9be7d793be6ea75a6b05
José Henrique <josephenrycastro@gmail.com>
2026-08-18 16:25:47 -0300

    feat: routes for profile,comments,like,edit and update

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use App\Models\Post;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\ProfileController;
 
 Route::middleware('guest')->group(function (){
 
@@ -29,5 +32,15 @@ Route::post('/posts',[PostController::class,'store'])->name('posts.store');
 Route::delete('/posts/{post}',[PostController::class,'destroy'])->name('posts.destroy');
 
 Route::post('/logout',[AuthController::class,'logout'])->name('logout');
+
+Route::resource('profiles',ProfileController::class)->only(['show','edit','update']); 
+
+Route::post('/posts/{post}/comments',[CommentController::class,'store'])->name('posts.comment');
+
+Route::post('/posts/{post}/like',[LikeController::class,'toggle'])->name('posts.like');
+
+Route::get('/posts/{post}/edit',[PostController::class,'edit'])->name('posts.edit');
+
+Route::put('/posts/{post}',[PostController::class,'update'])->name('posts.update');
 
 });
